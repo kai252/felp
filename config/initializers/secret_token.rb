b647dc028c52eac695045740db5c96a5d131caa3
Phillip <kai252@gmail.com>
2012-08-03 18:14:21 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Felp::Application.config.secret_token = 'ce857b171167afc643b78f963573c6f483a815dfec355b6bd41ce64c279c54d8e3d541883e4a412f3ab368bfa6edf3e601dc791112d22161666e1572af94639c'
